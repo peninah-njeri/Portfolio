67a53b77ace8b1ba9bcbe14e5d7657dfe17a32e0
peninah-njeri <peninahgathuru@gmail.com>
2020-01-19 15:36:12 +0300

    Create my project folder and an app

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/pesh/Documents/njeri-moringa-school-projects/Final/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
